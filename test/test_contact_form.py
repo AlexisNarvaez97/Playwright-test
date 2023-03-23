a1233d0b6cba34eff304092c3fc002ff4fdb25da
NarvaeZ <alexis.narvaez@umvel.com>
2023-03-23 13:04:56 -0600

    Add configurations files for execut

--- test/test_contact_form.py
+++ test/test_contact_form.py
@@ -1,7 +1,9 @@
 from playwright.sync_api import Playwright, sync_playwright
 from pom.contact_us_page import ContactPage
+import pytest
 
 
+@pytest.mark.regression
 def test_submit_form(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False, args=["--start-maximized", "--start-in-incognito"])
     context = browser.new_context(no_viewport=True)
--- test/test_home_page.py
+++ test/test_home_page.py
@@ -1,3 +1,4 @@
+import pytest
 from playwright.sync_api import Playwright, expect
 from pom.home_page_elements import HomePage
 
@@ -14,3 +15,17 @@ def test_about_us(playwright: Playwright) -> None:
     # ---------------------
     context.close()
     browser.close()
+
+
+def test_about_us_2(playwright: Playwright) -> None:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://symonstorozhenko.wixsite.com/website-1")
+        home_page = HomePage(page)
+        expect(home_page.celebrate_header).to_be_visible()
+        expect(home_page.celebrate_body).to_be_visible()
+
+        # ---------------------
+        context.close()
+        browser.close()
